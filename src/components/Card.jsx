50a758892418df3bf6fdda1513751e37b30b82a8
saksham-x <sakshampokharel2002@gmail.com>
2023-11-26 15:31:35 +0545

    Show more and less button

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
 
 const Card = (props) => {
+    const {image,title,price,id}=props.data
     return (
         <>
 
             <div className="col">
                 <div className="card">
-                    <img src={props.data.image} className="card-img-top" alt={props.data.title} />
+                    <img src={image} className="card-img-top" alt={props.data.title} />
                     <div className="card-body">
-                        <h5 className="card-title">{props.data.title}</h5>
-                        <h5>${props.data.price}</h5>
+                        <h5 className="card-title">{title.slice(0,20)}...</h5>
+                        <h5>${price}</h5>
                         <button type="button" class="btn btn-info">Details</button>
                     </div>
                 </div>
--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -4,6 +4,7 @@ import axios from "axios";
 
 const Products = () => {
   const [products, setProducts] = useState([])
+  const [limit, setLimit] = useState(12)
   useEffect(() => {
     axios.get(`https://fakestoreapi.com/products`)
       .then(res => {
@@ -17,11 +18,18 @@ const Products = () => {
 
       <div className="container-fluid">
         <div className="row row-cols-1 row-cols-md-4 g-4">
-          {products.map((item, i) => (
+          {products.slice(0, limit).map((item, i) => (
             <Card data={item} key={i} />
           ))}
 
-
+          <div className="d-flex justify-content-center my-3 mx-auto">
+            {limit<products.length &&
+              <button className="btn btn-danger" onClick={() => setLimit(limit + 4)}>Load more</button>
+            }
+            {limit > 4 &&
+              <button className="btn btn-danger" onClick={() => setLimit(limit - 4)}>Show less</button>
+            }
+          </div>
 
         </div>
       </div>
@@ -30,11 +38,8 @@ const Products = () => {
           <> 
            <h1>{item.title}</h1>
           <h2>${item.price}</h2>
-          <h3>{item.category}</h3> */}
-
-
-      )
-      })}
+          <h3>{item.category}</h3> )
+      })} */}
     </>
   )
 }
