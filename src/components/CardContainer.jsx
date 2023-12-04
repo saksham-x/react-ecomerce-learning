7757b99b407e53a8520ee7697bfa0df7a977100d
saksham-x <sakshampokharel2002@gmail.com>
2023-12-04 19:21:40 +0545

    done

--- src/components/CardContainer.jsx
+++ src/components/CardContainer.jsx
@@ -1,52 +1,55 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { ColorRing } from "react-loader-spinner";
-import Card from './Card';
+import Card from "./Card";
 const CardContainer = () => {
-    const [products, setProducts] = useState([])
-    const [loading, setLoading] = useState(true)
-    useEffect(() => {
-        const fetchProduct = async () => {
-            try {
-                const response = await axios.get(`https://fakestoreapi.com/products`)
-                console.log(response.data);
-                setProducts(response.data)
-                setLoading(false)
-            }
-            catch (err) {
-                console.log(err);
-            }
-        }
-        //simulate 2 sec delay before featching the data
-        const delay = setTimeout(() => {
-            fetchProduct()
-            clearTimeout(delay)
-        }, 2000)
-    }, [])
-    return (
-        <>
-            {loading ? (
-                <div className='d-flex justify-content-center align-items-center' style={{ height: '50vh' }}>
-                    <ColorRing
-                        visible={true}
-                        height="80"
-                        width="80"
-                        ariaLabel="blocks-loading"
-                        wrapperStyle={{}}
-                        wrapperClass="blocks-wrapper"
-                        colors={['#e15b64', '#f47e60', '#f8b26a', '#abbd81', '#849b87']}
-                    />
-
-                </div>
-
-            ) : (
-               products.map((item,i)=>(
-                <Card key = {i} data={item} />
-               )
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    const fetchProduct = async () => {
+      try {
+        const response = await axios.get(`https://fakestoreapi.com/products`);
+        console.log(response.data);
+        setProducts(response.data);
+        setLoading(false);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    //simulate 2 sec delay before featching the data
+    const delay = setTimeout(() => {
+      fetchProduct();
+      clearTimeout(delay);
+    }, 2000);
+  }, []);
+  return (
+    <>
+      {loading ? (
+        <div
+          className="d-flex justify-content-center align-items-center"
+          style={{ height: "50vh" }}
+        >
+          <ColorRing
+            visible={true}
+            height="80"
+            width="80"
+            ariaLabel="blocks-loading"
+            wrapperStyle={{}}
+            wrapperClass="blocks-wrapper"
+            colors={["#e15b64", "#f47e60", "#f8b26a", "#abbd81", "#849b87"]}
+          />
+        </div>
+      ) : (
+        <div className="container-fluid">
+          <div className="row row-cols-1 row-cols-md-4 g-4">
+          {  products.slice(0,8).map((item,i)=>(
+            <Card key={i} data={item} />
             ))}
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
 
-        </>
-    )
-}
-
-export default CardContainer
+export default CardContainer;
